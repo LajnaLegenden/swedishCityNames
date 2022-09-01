05b35571cbe8947b6e8980c5c24d92dde0885d42
Linus jansson <00jansson@gmail.com>
2022-09-01 23:51:28 +0200

    Update

--- script.js
+++ script.js
@@ -4,10 +4,39 @@ const letters = "abcdefghijklmnopqrstuvwxyzåäö"
 
 ortNamn = []
 
-async function main() {
+async function getCities() {
+
+    for (let i = 0; i < letters.length; i++) {
+        console.log("Getting cities for ", letters[i])
+        await fetch("https://ortnamn.lantmateriet.se/api/ortnamnsregistret/ortnamn/kriterier/v1?namn=" + letters[i] + "&match=startsWith&namntyp=T%C3%A4tort&transactionId=1", {
+            "headers": {
+                "accept": "application/json, text/plain, */*",
+                "cookie": "cookieConsentOrtnamn=false",
+            },
+            "method": "GET"
+        }).then(async (res) => {
+            if (res.status != 200) {
+                console.log("error on", letters[i])
+                return
+            }
+            let parsed;
+            try {
+                parsed = await res.json()
+                ortNamn = [...ortNamn, parsed.ortnamnSearchResponseItems]
+
+            } catch (error) {
+                console.log("error", error.message, res)
+            }
+        })
+
+    }
+    fs.writeFileSync("cities.json", JSON.stringify(ortNamn, null, 2), { flag: "w" })
+}
+
+async function getEverything() {
     for (let i = 0; i < letters.length; i++) {
         for (let j = 0; j < letters.length; j++) {
-            console.log("getting names for ", letters[i] + letters[j])
+            console.log("Getting everything for ", letters[i] + letters[j])
             await fetch("https://ortnamn.lantmateriet.se/api/ortnamnsregistret/ortnamn/kriterier/v1?namn=" + letters[i] + letters[j] + "&match=startsWith&transactionId=1", {
                 "headers": {
                     "accept": "application/json, text/plain, */*",
@@ -30,9 +59,11 @@ async function main() {
             })
         }
     }
-    fs.writeFileSync("cities.json", JSON.stringify(ortNamn, null, 2), { flag: "w" })
-
+    fs.writeFileSync("everything.json", JSON.stringify(ortNamn, null, 2), { flag: "w" })
 
 }
 
+async function main() {
+    await Promise.all([getEverything(), getCities()])
+}
 main()
